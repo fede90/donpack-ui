58b84f8432bd336fd0cdc5e80080f3f0f212c36b
frojo <rojo.fede@gmail.com>
2023-11-06 00:22:41 -0300

    project configuration

--- next.config.js
+++ next.config.js
@@ -1,4 +1,28 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+const ESLintPlugin = require('eslint-webpack-plugin');
 
-module.exports = nextConfig
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    webpack: (config,env) => {
+        if (env.dev) {
+            config.plugins.push(new ESLintPlugin({
+                extensions: ['js', 'jsx', 'ts', 'tsx'],
+                files: ['src'],
+                failOnError: false,
+            }))
+        }
+        return config;
+    },
+    images: {
+        remotePatterns: [
+          {
+            protocol: 'https',
+            hostname: 'images.unsplash.com',
+            port: '',
+            pathname: '/photo**',
+          },
+        ],
+      },
+
+};
+
+module.exports = nextConfig;
